556a32eef2e263b5da2fb4a0ea693b7e98f24e21
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-10 14:17:01 -0600

    Display modal view for detail screen

--- Rogram/PhotoDetailView.swift
+++ Rogram/PhotoDetailView.swift
@@ -4,10 +4,20 @@ struct PhotoDetailView: View {
 	let photo: Photo
 
     var body: some View {
-		AsyncImage(url: photo.detailImageUrl)
-			.frame(width: 600, height: 600)
-		Text(photo.title).textCase(.uppercase)
-			.font(.headline)
+		VStack {
+			AsyncImage(url: photo.detailImageUrl) { image in
+				image.resizable()
+			} placeholder: {
+				Image(systemName: "photo")
+					.resizable()
+					.symbolRenderingMode(.hierarchical)
+					.foregroundStyle(Color.gray.opacity(0.3))
+			}
+			.frame(width: 500, height: 500)
+			Text(photo.title).textCase(.uppercase)
+				.font(.headline)
+				.padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 12))
+		}
     }
 }
 
--- Rogram/PhotosListView.swift
+++ Rogram/PhotosListView.swift
@@ -2,18 +2,33 @@ import SwiftUI
 
 struct PhotosListView: View {
 	@StateObject var viewModel = PhotosListViewModel()
-	
+	@State private var isShowingDetailView: Bool = false
+	@State private var selectedPhotoId: Int?
+
 	var body: some View {
-		List(viewModel.photosList) { item in
-			PhotosRowCell(photo: item)
-				.alignmentGuide(.listRowSeparatorLeading, computeValue: { dimension in
-					dimension[.leading]
-				})
-				.listRowSeparatorTint(.secondary)
-				.listStyle(.plain)
-		}
-		.task {
-			await viewModel.fetchAllPhotos()
+		NavigationStack {
+			List(viewModel.photosList, selection: $selectedPhotoId) { item in
+				PhotosRowCell(photo: item)
+					.onTapGesture {
+						selectedPhotoId = item.id
+						isShowingDetailView = true
+					}
+					.alignmentGuide(.listRowSeparatorLeading, computeValue: { dimension in
+						dimension[.leading]
+					})
+					.listStyle(.plain)
+					.listRowBackground(Color.clear)
+			}
+			.task {
+				await viewModel.fetchAllPhotos()
+			}
+			.navigationTitle("Ro")
+			.background(Color.clear)
+			.sheet(isPresented: $isShowingDetailView) {
+				if let selectedPhoto = viewModel.photosList.first(where: { $0.id == selectedPhotoId }) {
+					PhotoDetailView(photo: selectedPhoto)
+				}
+			}
 		}
 	}
 }
