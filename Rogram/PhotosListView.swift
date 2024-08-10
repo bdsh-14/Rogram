708d2b35d5fdc740248603f42a374eb07d619f99
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-10 12:56:26 -0600

    Display data from network call

--- Rogram/PhotosListView.swift
+++ Rogram/PhotosListView.swift
@@ -1,16 +1,21 @@
 import SwiftUI
 
 struct PhotosListView: View {
-    var body: some View {
-		List(Photo.mockData) { item in
+	@StateObject var viewModel = PhotosListViewModel()
+	
+	var body: some View {
+		List(viewModel.photosList) { item in
 			PhotosRowCell(picture: item)
 				.alignmentGuide(.listRowSeparatorLeading, computeValue: { dimension in
 					dimension[.leading]
 				})
-				.listRowSeparatorTint(.red)
-
-        }
-    }
+				.listRowSeparatorTint(.secondary)
+				.listStyle(.plain)
+		}
+		.task {
+			await viewModel.fetchAllPhotos()
+		}
+	}
 }
 
 #Preview {
--- Rogram/PhotosRowCell.swift
+++ Rogram/PhotosRowCell.swift
@@ -5,21 +5,23 @@ struct PhotosRowCell: View {
 
     var body: some View {
 		VStack {
-			AsyncImage(url: URL(string: picture.thumbnailUrlString)!)
+			AsyncImage(url: picture.thumbnailUrl)
 				.frame(width: 300, height: 400)
-				.background(Color.pink.opacity(0.5))
+				.border(Color.secondary)
 			HStack {
 				Text(picture.title).textCase(.uppercase)
 					.font(.subheadline)
+
 				Spacer()
 				Button("", systemImage: "heart") {
 					print("Button pressed")
 				}
 			}
+			.padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 12))
 		}
     }
 }
 
 #Preview {
-	PhotosRowCell(picture: Photo.mockData[0])
+	PhotosRowCell(picture: mockPhotos[0])
 }
