2e25e2fc6f29e8f75ab1df9ed3a6fd2456e00a7e
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-10 14:33:09 -0600

    Fix heart to match wireframe

--- Rogram/PhotosRowCell.swift
+++ Rogram/PhotosRowCell.swift
@@ -8,26 +8,23 @@ struct PhotosRowCell: View {
 			AsyncImage(url: photo.thumbnailUrl)
 				.frame(width: 300, height: 400)
 				.border(Color.secondary)
-			HStack {
+			HStack(alignment: .firstTextBaseline)
+			{
 				Text(photo.title).textCase(.uppercase)
 					.font(.subheadline)
 
 				Spacer()
-				Button {
-					print("Button pressed")
-				} label: {
-					Label(
-						title: { Text("") },
-						icon: {
-							Image(systemName: "heart.fill")
-								.resizable()
-								.frame(width: 20, height: 20)
-								.symbolRenderingMode(.multicolor)
-								.fontWeight(.bold)
-								.foregroundStyle(Color.red)
-						}
-					)
-				}
+				Label(
+					title: { Text("") },
+					icon: {
+						Image(systemName: "heart.fill")
+							.resizable()
+							.frame(width: 20, height: 20)
+							.symbolRenderingMode(.multicolor)
+							.fontWeight(.bold)
+							.foregroundStyle(Color.red)
+					}
+				)
 			}
 			.padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 12))
 		}
