e4b41f49561069f34309d4493f7aebc7967203b5
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-10 10:44:19 -0600

    Initial Commit

---
+++ Rogram/RogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogramApp.swift
+//  Rogram
+//
+//  Created by Bidisha Biswas on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
