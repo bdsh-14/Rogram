b99b936bf2f6f3f939c4cb1686aab49bf5f37ba1
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-10 20:31:21 -0600

    Create subview for image title in detail view

---
+++ Rogram/PhotosDetail/TitleView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct TitleView: View {
+	let title: String
+
+	var body: some View {
+		HStack(alignment: .firstTextBaseline)
+		{
+			Text(title).textCase(.uppercase)
+				.font(.subheadline)
+
+			Spacer()
+			Label(
+				title: { Text("") },
+				icon: {
+					Image(systemName: "heart.fill")
+						.resizable()
+						.frame(width: 20, height: 20)
+						.symbolRenderingMode(.multicolor)
+						.fontWeight(.bold)
+						.foregroundStyle(Color.red)
+				}
+			)
+		}
+		.padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 12))
+	}
+}
--- Rogram/PhotosList/PhotosRowCell.swift
+++ Rogram/PhotosList/PhotosRowCell.swift
@@ -8,25 +8,7 @@ struct PhotosRowCell: View {
 			RemoteImage(url: photo.thumbnailUrl,
 						loadingView: PhotosRowLoadingCell())
 				.frame(width: 300, height: 400)
-			HStack(alignment: .firstTextBaseline)
-			{
-				Text(photo.title).textCase(.uppercase)
-					.font(.subheadline)
-
-				Spacer()
-				Label(
-					title: { Text("") },
-					icon: {
-						Image(systemName: "heart.fill")
-							.resizable()
-							.frame(width: 20, height: 20)
-							.symbolRenderingMode(.multicolor)
-							.fontWeight(.bold)
-							.foregroundStyle(Color.red)
-					}
-				)
-			}
-			.padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 12))
+			TitleView(title: photo.title)
 		}
 	}
 }
