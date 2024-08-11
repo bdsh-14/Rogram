851729ea0469bb34679bc300e0b844df6c29d29d
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-10 21:48:44 -0600

    Add network manager mock

--- RogramTests/MockNetworkManager.swift
+++ RogramTests/MockNetworkManager.swift
@@ -1,8 +1,17 @@
-//
-//  MockNetworkManager.swift
-//  RogramTests
-//
-//  Created by Bidisha Biswas on 8/10/24.
-//
-
 import Foundation
+@testable import Rogram
+
+class MockNetworkManager: NetworkManagerInterface {
+	var shouldReturnError = false
+
+	func fetchPhotosList() async throws -> [Photo] {
+		if shouldReturnError {
+			throw URLError(.badServerResponse)
+		} else {
+			return [
+				Photo(id: 1, title: "Test Photo 1", urlString: "https://example.com/photo1.jpg", thumbnailUrlString: "https://example.com/photo1_thumb.jpg"),
+				Photo(id: 2, title: "Test Photo 2", urlString: "https://example.com/photo2.jpg", thumbnailUrlString: "https://example.com/photo2_thumb.jpg")
+			]
+		}
+	}
+}
